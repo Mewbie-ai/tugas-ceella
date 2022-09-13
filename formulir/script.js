fff06c800c7bdd53cc15bcf45b43075bbfa91026
unknown <nicoalexandrekurniawan@gmail.com>
2022-09-13 14:14:59 +0700

    Merapikan tampilan

--- formulir/script.js
+++ formulir/script.js
@@ -1,12 +1,10 @@
 function changeLiputan() {
     document.getElementById('liputan').style.display = "block";
     document.getElementById('publikasi').style.display = "none";
-    document.getElementById('kotak_liputan').style.display = "block";
-    document.getElementById('kotak_publikasi').style.display = "none";
+    document.getElementById('titleForm').innerHTML = "Form Liputan";
 }
 function changePublikasi() {
     document.getElementById('liputan').style.display = "none";
     document.getElementById('publikasi').style.display = "block";
-    document.getElementById('kotak_publikasi').style.display = "block";
-    document.getElementById('kotak_liputan').style.display = "none";
+    document.getElementById('titleForm').innerHTML = "Form Publikasi";
 }
